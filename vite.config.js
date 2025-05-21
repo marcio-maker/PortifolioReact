47f3712a8970563b6d79c74ddc40fb57a31ff3c0
Marcio Maker <you@example.com>
2025-05-20 22:50:51 -0300

    Adiciona .gitignore e limpa arquivos rastreados

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/PortifolioReact/', // ⚠️ Use o nome CERTO do repositório!
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
-})
+});
