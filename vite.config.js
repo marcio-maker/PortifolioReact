9cbc158bbcc50035fb5598b9987781868d4c9fd1
Marcio Dev <79775392+marcio-maker@users.noreply.github.com>
2025-05-21 22:47:09 -0300

    Update vite.config.js
    up

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Use exatamente o mesmo nome do seu repositório GitHub
 export default defineConfig({
-  base: '/PortfolioReact/', // Ou '/PortifolioReact/' se for o nome correto
+  base: '/PortfolioReact/', // Confirme o nome exato do seu repositório
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  },
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     assetsDir: 'assets'
   }
 });
